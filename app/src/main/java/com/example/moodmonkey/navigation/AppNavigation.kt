aeb78ab38c62fe060a2d473763231eb167c16a32
Bambi <milan.t.kadri@gmail.com>
2025-02-05 09:54:39 +0100

    monkeyboss organization

---
+++ app/src/main/java/com/example/moodmonkey/navigation/AppNavigation.kt
@@ -0,0 +1,83 @@
+package com.example.moodmonkey.navigation
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavGraph.Companion.findStartDestination
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.moodmonkey.viewModel.MoodEntryViewModel
+import com.example.moodmonkey.views.MoodEntryListView
+import com.example.moodmonkey.views.NewEntry
+import com.example.moodmonkey.views.StatisticView
+
+@Composable
+fun AppNavigation() {
+    val viewModel: MoodEntryViewModel = viewModel()
+    val moodList by viewModel.allMoods.collectAsState()
+    var selectedNavItems by remember { mutableStateOf(NavItem.First) }
+    val navController: NavHostController = rememberNavController()
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        bottomBar = {
+            NavigationBar {
+                NavItem.entries.forEach { item ->
+                    NavigationBarItem(
+                        selected = selectedNavItems == item,
+                        onClick = {
+                            selectedNavItems = item
+                            navController.popBackStack()
+                            navController.navigate(item.route) {
+                                popUpTo(navController.graph.findStartDestination().id) {
+                                    saveState = true
+                                }
+                            }
+                        },
+                        icon = {
+                            Icon(painter = painterResource(id = item.icon), item.label)
+                        },
+                        label = { Text(item.label) })
+                }
+            }
+        }
+    ) { innerPadding ->
+
+        NavHost(
+            startDestination = DashboardRoute,
+            navController = navController,
+            modifier = Modifier.padding(innerPadding),
+
+            ) {
+
+            composable<DashboardRoute> {
+                MoodEntryListView(
+                    entries = moodList
+                )
+            }
+
+            composable<NewEntryRoute> {
+                NewEntry()
+            }
+
+            composable<StatisticsRoute> {
+                StatisticView()
+            }
+        }
+    }
+
+}
--- app/src/main/java/com/example/moodmonkey/views/newEntryView.kt
+++ app/src/main/java/com/example/moodmonkey/views/newEntryView.kt
@@ -1,11 +1,28 @@
 package com.example.moodmonkey.views
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 
 
 @Composable
 fun NewEntry(modifier: Modifier = Modifier) {
+
     Text("New Entry")
+
+    Column(modifier = modifier.fillMaxSize()) {
+
+        // Slider
+
+        // ActivityList LazyRow
+
+        // Content Multiline TextField
+
+        // Save Button -> Creates new MoodEntry with Date
+
+    }
 }
