30d2187f1033e66f7d9948716c138f770bb3bc5c
Markus Wirtz <info@markuswirtz.net>
2025-02-06 14:30:55 +0100

    Uhrzeit in ListView, Icons für Activities, Coderefactoring...

--- app/src/main/java/com/example/moodmonkey/views/Components/MoodEntryCardView.kt
+++ app/src/main/java/com/example/moodmonkey/views/Components/MoodEntryCardView.kt
@@ -1,7 +1,9 @@
+import android.R.attr.fontWeight
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.*
@@ -9,7 +11,9 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
@@ -28,7 +32,9 @@ fun MoodEntryCardView(
     var showContent by remember { mutableStateOf(false) }
 
     Card(
-        modifier = Modifier.fillMaxWidth().padding(8.dp),
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(8.dp),
         shape = RoundedCornerShape(12.dp),
         elevation = CardDefaults.cardElevation(defaultElevation = 5.dp),
         colors = CardDefaults.cardColors(secondaryContainerLight)
@@ -43,13 +49,15 @@ fun MoodEntryCardView(
                 verticalAlignment = Alignment.CenterVertically
             ) {
                 Text(
-                    text = entry.moodEntryDate,
+                    text = "Date: ${entry.moodEntryDate}",
                     fontSize = MaterialTheme.typography.bodyLarge.fontSize,
                     fontWeight = FontWeight.Bold,
                     color = Color.White
                 )
-                Spacer(modifier = Modifier
-                    .width(8.dp))
+                Spacer(
+                    modifier = Modifier
+                        .width(8.dp)
+                )
             }
 
             Row(
@@ -58,18 +66,22 @@ fun MoodEntryCardView(
                     .padding(16.dp)
             ) {
                 Image(
-                    painter = painterResource(id = R.drawable.kongtainer),
+                    painter = painterResource(id = R.drawable.amazing),
                     contentDescription = null,
                     modifier = Modifier
                         .size(60.dp)
                         .clip(CircleShape)
                 )
 
-                Spacer(modifier = Modifier
-                    .width(12.dp))
+                Spacer(
+                    modifier = Modifier
+                        .width(12.dp)
+                )
 
-                Column(modifier = Modifier
-                    .weight(1f)) {
+                Column(
+                    modifier = Modifier
+                        .weight(1f)
+                ) {
                     Row(
                         verticalAlignment = Alignment.CenterVertically,
                         horizontalArrangement = Arrangement.SpaceBetween,
@@ -79,13 +91,13 @@ fun MoodEntryCardView(
                         Text(
                             text = entry.moodEntryTitle,
                             fontWeight = FontWeight.Bold,
-                            fontSize = MaterialTheme.typography.bodyLarge.fontSize,
+                            fontSize = MaterialTheme.typography.titleLarge.fontSize,
                         )
                         Row(verticalAlignment = Alignment.CenterVertically) {
-                            Text(
-                                //Todo: Uhrzeit aktuell hartgecodet
 
-                                text = "⏰ 12:30",
+                            Icon(painter = painterResource(id = R.drawable.baseline_access_time_24), contentDescription = null)
+                            Text(
+                                text = entry.moodEntryTime,
                                 fontSize = MaterialTheme.typography.bodyLarge.fontSize,
                             )
                             IconButton(
@@ -101,47 +113,80 @@ fun MoodEntryCardView(
                         }
                     }
 
-                    Spacer(modifier = Modifier
-                        .height(8.dp))
-
-                    Row(
+                    Spacer(
                         modifier = Modifier
-                            .fillMaxWidth(),
-                        horizontalArrangement = Arrangement.SpaceEvenly
-                    ) {
+                            .height(8.dp)
+                    )
 
-                        //Todo: Activities aus der Datenbank für dieses Entrie lesen und ausgeben (forEach/Items)
 
-                        Icon(
-                            painter = painterResource(id = R.drawable.baseline_add_circle_outline_24),
-                            contentDescription = null,
-                            modifier = Modifier
-                                .size(24.dp)
-                        )
-                        Icon(
-                            painter = painterResource(id = R.drawable.baseline_add_circle_outline_24),
-                            contentDescription = null,
-                            modifier = Modifier
-                                .size(24.dp)
-                        )
-                        Icon(
-                            painter = painterResource(id = R.drawable.baseline_add_circle_outline_24),
-                            contentDescription = null,
-                            modifier = Modifier
-                                .size(24.dp)
-                        )
-                    }
 
-                    Spacer(modifier = Modifier
-                        .height(8.dp))
-
-                    Spacer(modifier = Modifier
-                        .height(8.dp))
+                    Spacer(
+                        modifier = Modifier
+                            .height(8.dp)
+                    )
 
                     AnimatedVisibility(
                         visible = showContent
                     ) {
                         Column {
+
+                            Row(
+                                modifier = Modifier
+                                    .fillMaxWidth(),
+                                horizontalArrangement = Arrangement.Start
+                            ) {
+
+                                //Todo: Activities aus der Datenbank für dieses Entrie lesen und ausgeben (forEach/Items)
+
+                                Image(
+                                    painter = painterResource(R.drawable.baseline_circle_24),
+                                    contentDescription = "", contentScale = ContentScale.Fit,
+                                    modifier = Modifier.padding(horizontal = 2.dp).width(28.dp).height(28.dp)
+                                )
+                                Image(
+                                    painter = painterResource(R.drawable.baseline_circle_24),
+                                    contentDescription = "",contentScale = ContentScale.Fit,
+                                    modifier = Modifier.padding(horizontal = 2.dp).width(28.dp).height(28.dp)
+                                )
+                                Image(
+                                    painter = painterResource(R.drawable.baseline_circle_24),
+                                    contentDescription = "",contentScale = ContentScale.Fit,
+                                    modifier = Modifier.padding(horizontal = 2.dp).width(28.dp).height(28.dp)
+                                )
+                                Image(
+                                    painter = painterResource(R.drawable.baseline_circle_24),
+                                    contentDescription = "",contentScale = ContentScale.Fit,
+                                    modifier = Modifier.padding(horizontal = 2.dp).width(28.dp).height(28.dp)
+                                )
+                                Image(
+                                    painter = painterResource(R.drawable.baseline_circle_24),
+                                    contentDescription = "", contentScale = ContentScale.Fit,
+                                    modifier = Modifier.padding(horizontal = 2.dp).width(28.dp).height(28.dp)
+                                )
+                                Image(
+                                    painter = painterResource(R.drawable.baseline_circle_24),
+                                    contentDescription = "",contentScale = ContentScale.Fit,
+                                    modifier = Modifier.padding(horizontal = 2.dp).width(28.dp).height(28.dp)
+                                )
+                                Image(
+                                    painter = painterResource(R.drawable.baseline_circle_24),
+                                    contentDescription = "",contentScale = ContentScale.Fit,
+                                    modifier = Modifier.padding(horizontal = 2.dp).width(28.dp).height(28.dp)
+                                )
+                                Image(
+                                    painter = painterResource(R.drawable.baseline_circle_24),
+                                    contentDescription = "",contentScale = ContentScale.Fit,
+                                    modifier = Modifier.padding(horizontal = 2.dp).width(28.dp).height(28.dp)
+                                )
+
+                            }
+
+                            Spacer(
+                                modifier = Modifier
+                                    .height(8.dp)
+                            )
+
+
                             HorizontalDivider(
                                 modifier = Modifier
                                     .fillMaxWidth(),
